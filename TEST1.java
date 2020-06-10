309e85c7a8330580c2d49f3fc43774d9e2a08374
lichenwei5210 <13060338209@163.com>
2020-06-10 17:11:40 +0800

    java判断语句

---
+++ TEST1.java
@@ -0,0 +1,19 @@
+package org_yuan.my_project;
+
+public class TEST1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int s = 61;
+		if(s>90) {
+			System.out.print("考的非常好");
+		}
+		else if(s>60){
+			System.out.print("刚及格");
+		}
+		else {
+			System.out.print("不及格");
+		}
+	}
+
+}
